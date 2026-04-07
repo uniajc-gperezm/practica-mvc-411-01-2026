a49422d72ec57b49962a2bb13efd062da9ef9524
Christian <christian.h0701@gmail.com>
2026-04-06 20:28:08 -0500

    Agregando nuevo metodo en la vista estudiante

--- src/main/java/com/uniajc/vista/VistaEstudiante.java
+++ src/main/java/com/uniajc/vista/VistaEstudiante.java
@@ -1,5 +1,26 @@
 package com.uniajc.vista;
 
+import java.util.List;
+
+import com.uniajc.modelo.Estudiante;
+
 public class VistaEstudiante {
 
+    // La manera sencilla de mostrar los detalles de un estudiante atraves de un metodo que reciba un objeto
+    //public void mostrarDetallesEstudiante(String nombre, int edad){
+    //    System.out.println("nombre: " + nombre);
+    //    System.out.println("Edad: " + edad);
+    //}
+
+    public void mostrarDetallesEstudiante(Estudiante estudiante){
+        System.out.println("Nombre: " + estudiante.getNombre() + ", Edad: " + estudiante.getEdad());
+
+    }
+
+    public void mostrarTodosEstudiantes(List<Estudiante> estudiantes){
+        System.out.println("Lista de Estudiantes: ");
+        for (Estudiante estudiante : estudiantes){
+            mostrarDetallesEstudiante(estudiante);
+        }
+    }
 }
