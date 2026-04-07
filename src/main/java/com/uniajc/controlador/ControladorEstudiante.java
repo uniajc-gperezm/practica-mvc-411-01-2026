2040b6667039f0e4e89840578958011342b49f3d
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2026-04-06 20:19:43 -0500

    Agregando controlador estudiante

---
+++ src/main/java/com/uniajc/controlador/ControladorEstudiante.java
@@ -0,0 +1,38 @@
+package com.uniajc.controlador;
+
+import com.uniajc.modelo.Estudiante;
+import com.uniajc.vista.VistaEstudiante;
+
+public class ControladorEstudiante {
+
+    private VistaEstudiante vista;
+    private Estudiante modelo;
+
+    public ControladorEstudiante(VistaEstudiante vista, Estudiante modelo) {
+        this.vista = vista;
+        this.modelo = modelo;
+    }
+
+   public String getNombre() {
+        return modelo.getNombre();
+    }
+
+    public void setNombre(String nombre) {
+        modelo.setNombre(nombre);
+    }
+
+    public int getEdad() {
+        return modelo.getEdad();
+    }
+
+    public void setEdad(int edad) {
+        modelo.setEdad(edad);
+    }
+
+    public void actualizarVista() {
+        vista.mostrarDetallesEstudiante(modelo);
+    }
+
+    
+
+}
