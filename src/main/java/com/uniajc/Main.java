09d165396a287531bcbaffd1ea9c236377657794
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2026-04-06 20:21:12 -0500

    Probando funcionalidad del mvc

--- src/main/java/com/uniajc/Main.java
+++ src/main/java/com/uniajc/Main.java
@@ -1,7 +1,32 @@
 package com.uniajc;
 
+
+import com.uniajc.controlador.ControladorEstudiante;
+import com.uniajc.modelo.Estudiante;
+import com.uniajc.vista.VistaEstudiante;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Practica MVC - Uniajc!");
+
+        // Creando un estudiante desde su modelo
+        Estudiante estudiante = new Estudiante();
+
+        // Creando la vista de estudiante
+        VistaEstudiante vista = new VistaEstudiante();
+
+        // Creando el controlador de estudiante y vinculándolo con la vista y el modelo
+        ControladorEstudiante controlador = new ControladorEstudiante(vista, estudiante);
+
+        // Mostrando los detalles del estudiante a través del controlador
+        controlador.actualizarVista();
+
+        // Actualizando los datos del estudiante a través del controlador
+        controlador.setNombre("Maria Gomez");
+        controlador.setEdad(22);
+
+        // Actualizando la vista con los nuevos datos
+        controlador.actualizarVista();
+
     }
 }
