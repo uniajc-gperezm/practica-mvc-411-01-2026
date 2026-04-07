7e65310402d031846fc58285d8ea1bdedbdd34a8
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2026-04-06 20:27:45 -0500

    Agregando nuevo metodo en la vista estudiante para mostrar todos los estudiantes

--- src/main/java/com/uniajc/vista/VistaEstudiante.java
+++ src/main/java/com/uniajc/vista/VistaEstudiante.java
@@ -1,5 +1,7 @@
 package com.uniajc.vista;
 
+import java.util.List;
+
 import com.uniajc.modelo.Estudiante;
 
 public class VistaEstudiante {
@@ -7,6 +9,13 @@ public class VistaEstudiante {
     // La manera sencilla de mostrar los detalles de un estudiante es a través de un método que reciba un objeto Estudiante y acceda a sus propiedades para mostrarlas. Aquí te dejo un ejemplo de cómo podrías implementar esto:
     public void mostrarDetallesEstudiante(Estudiante estudiante) {
         System.out.println("Nombre: " + estudiante.getNombre() + ", Edad: " + estudiante.getEdad());
+    }
+
+    public void mostrarTodosLosEstudiantes(List<Estudiante> estudiantes) {
+        System.out.println("Lista de Estudiantes:");
+        for (Estudiante estudiante : estudiantes) {
+            mostrarDetallesEstudiante(estudiante);
+        }
     }
 
 }
